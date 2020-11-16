11864b6529f664af7917c78f8415eac2fa0e0ee2
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2020-11-15 19:21:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,52 @@
 # This is pre-in-dev 1
+# I am missing a lot of bank information on here, but fear not!
+# I am tirelessly (when I can) working on moving all of the
+# sensitive material into one file, and make this as simple
+# and autonomous as possible.  Give me a break, being a 
+# stay-at-home step dad to a 4-year-old girl is quite time-
+# consuming!  lol
 
-import time
 from time import sleep
-from datetime import date
-from sensitive import password_test, save_current_data, calculate_remaining_today, calculate_net_worth
-from budget_bug import budget_bugger
+from sensitive import password_test, check_usaa_balance
 from system_commands import clear_screen, bash_command
+import budget_bug
+from maths import current_date, current_time
+from important_functions import create_new_user, load_user_session
 
 clear_screen()
-t = time.localtime()
-current_time = time.strftime("%H:%M:%S", t)
-today = date.today()
-current_date = today.strftime("%m/%d/%Y")
 wrong_user_count = 0
+# ------------------------------'failed' to 'passed', False to True for debugging------------------------------
+log_in = 'failed'
+user = 'h@ck3r'
 authenticated = False
 
 while not authenticated:
+    print("""Welcome to "The Money Manager"!""")
+    granted_entry = False
+    while not granted_entry:
+        entry_point = input("""Are you returning user?  (Yes or No, non-case-sensitive)
+> """).lower()
+        clear_screen()
+        if entry_point == "yes":
+            granted_entry = True
+        elif entry_point == "no":
+            create_new_user()
+            granted_entry = True
+        elif entry_point == "exit":
+            break
+        else:
+            print(f"'{entry_point}' is neither yes, nor no, please try again.")
+
     incorrect_multiplier = 5
+
     user = input("Username: ")
-# "YourName" is your name, but of course this login method will change in the near future
-    if user == "YourName":
+    auth = load_user_session(user)
+
+    if auth == "legit":
         log_in = password_test()
         authenticated = True
     else:
-        print(f"""I do not recognise you, "{user}", go away!""")
+        print(f"""I do not recognise you, "{user}", please type carefully!""")
         wrong_user_count += 1
         seconds_to_wait = wrong_user_count * incorrect_multiplier
         print(f"Waiting {seconds_to_wait} seconds before retry allowed...")
@@ -42,12 +65,11 @@ if log_in == "passed":
         clear_screen()
 
         if command == "save":
-            save_current_data()
+            bash_command(f"echo '{current_date}@{current_time}' >> randomSave.txt")
         elif command == "--help":
             print(f"""List of commands:
 
-save  -  This will allow you to save a .json file with a line of text in it.
-You can give it both a [name].json and the text to be inserted within "".
+save  -  This will take all of the current data and store it with a time/date stamp.
 
 :  -  This will allow you to run a shell command.
 
@@ -55,9 +77,13 @@ You can give it both a [name].json and the text to be inserted within "".
 
 today  -  Will show today's remaining miscellaneous spending amount.
 
-net worth -  Will calculate and display your current net worth.
+budget  -  Runs the Budget Bugger application.
 
-exit  -  Closes the application.
+clear  -  Clears the screen.
+
+exit  -  Closes the application.  This command will be universal
+amongst all applications within this framework, to include the
+framework itself.
 
 --help  -  This will pull up the help menu you're seeing now.
 
@@ -65,18 +91,12 @@ NOTE: This terminal is NOT case-sensitive.""")
         elif command == ":":
             shell_command = input("root@kali:~#")
             bash_command(shell_command)
-# Random examples of banks, such as Chase, BoA, Cap1, etc.
-        elif command == "chase":
-            check_chase_balance()
-        elif command == "boa":
-            # --------------------THIS NOTE IS CENSORED TO THE PUBLIC--------------------
-            print("COMPLETE THIS SECTION")
         elif command == "today":
             print(calculate_remaining_today())
-        elif command == "net worth":
-            calculate_net_worth()
         elif command == "budget":
-            budget_bugger()
+            budget_bug.budget_bugger()
+        elif command == "clear":
+            clear_screen()
         elif command == "exit":
             break
         else:
