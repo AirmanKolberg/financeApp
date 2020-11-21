52fec5cf1b9811515b7e6c9cd179b3abd3029879
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2020-11-20 20:17:53 -0700

    Update system_commands.py

--- system_commands.py
+++ system_commands.py
@@ -5,5 +5,11 @@ def clear_screen():
     _ = system('clear')
 
 
-def bash_command(input):
-    _ = system(input)
+def bash_command(user_in):
+    _ = system(user_in)
+
+
+def use_terminal():
+    shell_command = input("root@kali:~# ")
+    bash_command(shell_command)
+    use_terminal()
