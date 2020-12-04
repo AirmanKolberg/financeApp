0f2329faa638f8a20cc9e2f3564d5738eccf24f8
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2020-12-03 18:33:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,17 @@
 # and autonomous as possible.  Give me a break, being a 
 # stay-at-home step dad to a 4-year-old girl is quite time-
 # consuming!  lol
-
 from time import sleep
 from system_commands import *
-import budget_bug
 from maths import *
 from important_functions import *
 
 clear_screen()
 wrong_user_count = 0
 # ------------------------------'failed' to 'passed', False to True for debugging------------------------------
-log_in = 'passed'
+log_in = 'failed'
 user = 'h@ck3r'
-authenticated = True
+authenticated = False
 
 while not authenticated:
     print("""Welcome to "The Financial Framework"!""")
@@ -59,49 +57,7 @@ if log_in == "passed":
 
 # --------------------INSIDE THE APP TERMINAL--------------------
 
-    while True:
-        command = input("> ").lower()
-        clear_screen()
-        if command == "save":
-            bash_command(f"echo '{current_date}@{current_time}' >> randomSave.txt")
-        elif command == "--help":
-            print(f"""List of commands:
-
-save  -  This will take all of the current data and store it with a time/date stamp.
-
-:  -  This will allow you to run a shell command.
-
-[bank]  -  Type the name of a bank to retrieve its balance.
-
-budget  -  Runs the Budget Bugger application.
-
-clear  -  Clears the screen.
-
-new pass  -  Select this option to choose a new password for your
-Financial Framework username.
-
-exit  -  Closes the application.  This command will be universal
-amongst all applications within this framework, to include the
-framework itself.
-
---help  -  This will pull up the help menu you're seeing now.
-
-NOTE: This terminal is NOT case-sensitive.""")
-        elif command == ":":
-            use_terminal()
-        elif command == "chase":
-            check_chase_balance()
-        elif command == "budget":
-            budget_bug.budget_bugger()
-        elif command == "new pass":
-            change_password(user)
-        elif command == "clear":
-            clear_screen()
-        elif command == "exit":
-            break
-        else:
-            print(f""""{command}" is not a valid command.
-Let's try something like this: --help""")
+    main_menu()
 
 else:
     print(f"Too many incorrect password attempts for {user}")
