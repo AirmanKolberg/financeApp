c00982ca897fa6fe2cc628076a48437c8e1c5898
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2020-12-14 20:00:46 -0700

    Update ew_sp500.py

--- ew_sp500.py
+++ ew_sp500.py
@@ -10,5 +10,12 @@ import pandas as pd
 import requests
 import xlsxwriter
 import math
+from secrets import IEX_CLOUD_API_TOKEN
 
 stocks = pd.read_csv('sp_500_stocks.csv')
+
+symbol = 'AAPL'
+api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote/?token={IEX_CLOUD_API_TOKEN}'
+
+data = requests.get(api_url)
+print(data.status_code)
