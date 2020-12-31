8f7ac7ba664e47c95a738be4fe4c117aa41e3d6c
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2020-12-31 16:02:08 -0700

    🤫 Secrets [EXAMPLE] File

--- secrets.py
+++ secrets.py
@@ -4,3 +4,12 @@
 
 # NOTE: This is not an actual token, my friend
 IEX_CLOUD_API_TOKEN = 'tHis_isWh3r3YouR-toK3/VG035'
+
+# These are again, of course, examples
+stocks_dictionary = {
+  'TSLA': 0,
+  'RTX': 1,
+  'HON': 2,
+  'KBR': 3,
+  'AAPL': 4
+}
